99d323c3fb47ce3f81b51651c07cf855b8d0fbfd
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-10-17 17:05:26 +0200

    Add testfunctions.

--- testfunctions.py
+++ testfunctions.py
@@ -1,17 +1,32 @@
 import numpy as np
-import scipy.optimize
+from ipdb import set_trace
 
-def testfunction_1d(x):
+def highfreq_1d(x):
     return np.exp(2*x) + 4*np.sin(5*x) + np.sin(80*x) + 10
 
+def lowfreq_1d(x):
+    return np.exp(2*x) + 4*np.sin(5*x) + 10
+
+def jump(x):
+    return np.piecewise(x,
+                        [
+                            x <= 0.5,
+                            x > 0.5
+                        ],
+                        [2, 4])
+
+
+
+# For 3d function coords is expected to be an n x 2 array.
+# [ [x1, y1], [x2, y2], [x3, y3], ...]
 
 def eggholder(coords):
     """ According to https://en.wikipedia.org/wiki/Test_functions_for_optimization
     Eggholder function shifted to [0,1]x[0,1]. """
     x = (coords[:,0] - 0.5) * 512
     y = (coords[:,1] - 0.5) * 512
-    
-    return -(y+47) * np.sin(np.sqrt(np.abs(x/2 + y+47))) - x*np.sin(np.sqrt(np.abs(x-(y+47))))
+        
+    return -(y+47) * np.sin(np.sqrt(np.abs(x/2 + y+47))) - x*np.sin(np.sqrt(np.abs(x-(y+47)))) + 400
 
 
 def rosenbrock(coords):
@@ -22,3 +37,35 @@ def rosenbrock(coords):
     y = (coords[:,1] - 0.5) * 4
     return np.square(a-x) + b * np.square((y-np.square(x)))
     # return scipy.optimize.rosen(coords)
+
+
+def platform(coords):
+    """ A jump between 4 different niveaus. """
+    vectors = np.piecewise(coords,
+                           [
+                               (coords[:,0] <= 0.5) & (coords[:,1] <= 0.5), # lower right
+                               (coords[:,0] <= 0.5) & (coords[:,1] > 0.5), # lower left
+                               (coords[:,0] > 0.5) & (coords[:,1] > 0.5), # upper left
+                               (coords[:,0] > 0.5) & (coords[:,1] <= 0.5)  # upper right
+                           ],
+                           [ 2, 5, 7, 8 ]
+    )
+    return vectors[:,0]
+
+
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    x = np.linspace(0, 1, 50)
+    y = np.linspace(0, 1, 50)
+
+    grid = np.meshgrid(x, y)
+    coords = np.vstack(np.meshgrid(x,y)).reshape(2, -1).T
+    z = platform(coords)
+    print(z)
+    plt.contourf(x, y, z.reshape(50,50))
+    plt.legend()
+    plt.show()
+    set_trace()
+    
+    
