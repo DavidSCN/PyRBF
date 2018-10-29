76756e97054a69c6a8636da6e59ed3e644350ac2
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-10-29 17:50:24 +0100

    Use ProcessPoolExecutor.

--- experiments/h_convergence.py
+++ experiments/h_convergence.py
@@ -3,10 +3,10 @@
 import concurrent.futures, itertools
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 import basisfunctions, rbf, testfunctions
-from ipdb import set_trace
 
 
-def kernel(mesh_size, RBF, bf, testfunction, m):
+def kernel(args):
+    mesh_size, RBF, bf, testfunction, m = args
     in_mesh = np.linspace(0, 1, num = int(mesh_size))
     test_mesh = np.linspace(0.1, 0.9, 50000)
 
@@ -22,13 +22,13 @@ def kernel(mesh_size, RBF, bf, testfunction, m):
              "RMSE" : interp.RMSE(testfunction, test_mesh),
              "InfError" : interp.error(testfunction, test_mesh),
              "ConditionC" : interp.condC,
-             "Testfunction" : str(f),
+             "Testfunction" : str(testfunction),
              "m" : m if bf.has_shape_param else 0}
 
 
 def main():
     # mesh_sizes = np.linspace(10, 5000, num = 50)
-    mesh_sizes = np.linspace(10, 50, num = 2)
+    mesh_sizes = np.linspace(10, 200, num = 2)
     bfs = [basisfunctions.Gaussian(), basisfunctions.ThinPlateSplines(),
                       basisfunctions.VolumeSplines(), basisfunctions.MultiQuadrics()]
     RBFs = [rbf.NoneConsistent, rbf.SeparatedConsistent]
@@ -42,29 +42,23 @@ def main():
             # skip iteration when the function has no shape parameter and it's not the first iteration in m
             continue
 
-        params.append({"mesh_size" : mesh_size,
-                       "RBF" : RBF,
-                       "bf" : bf,
-                       "testfunction" : tf,
-                       "m" : m})
+        params.append((mesh_size, RBF, bf, tf, m))
 
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers = 2) as executor:
-        result = executor.map(lambda a: kernel(**a), params)
+    with concurrent.futures.ProcessPoolExecutor(max_workers = 16) as executor:
+        result = executor.map(kernel, params)
     # for p in params:
         # kernel(**p)
 
 
-    df = pd.DataFrame(result)
+    df = pd.DataFrame(list(result))
     df = df.set_index("h")
 
     df.to_pickle("h_convergence.pkl")
     df.to_csv("h_convergence.csv")
-    set_trace()
 
     for name, group in df.groupby(["RBF", "BF", "Testfunction", "m"]):
         group.to_csv("h_convergence_" + "_".join(str(g) for g in name))
-        # print("h_convergence_" + "_".join(str(g) for g in name) + ".csv")
 
     print(df)
 
