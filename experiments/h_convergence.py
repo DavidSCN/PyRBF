fac358517e4aab75135b830b66fff90a041ec5d3
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2019-02-01 18:48:56 +0100

    Rewrite h_convergence.

--- experiments/h_convergence.py
+++ experiments/h_convergence.py
@@ -11,35 +11,45 @@ def kernel(args):
     test_mesh = np.linspace(0.1, 0.9, 50000)
 
     in_vals = testfunction(in_mesh)
-    b = bf.shaped(m, in_mesh)
+    b = bf(shape_parameter = bf.shape_param_from_m(m, in_mesh))
     
     interp = RBF(b, in_mesh, in_vals, rescale=False)
-    print(interp, testfunction, bf, "mesh_size = ", mesh_size, "m =", m)
+
+    # Compute the error here, for performance reasons
+    error = interp(test_mesh) - testfunction(test_mesh)
+    
+    print(interp, testfunction, b, "mesh_size =", mesh_size, "m =", m)
         
     return { "h" : 1 / mesh_size,
-            "RBF" : str(interp),
-             "BF" : str(bf),
-             "RMSE" : interp.RMSE(testfunction, test_mesh),
-             "InfError" : np.linalg.norm(interp.error(testfunction, test_mesh), ord=np.inf),
+             "RBF" : str(interp),
+             "BF" : str(b),
+             "RMSE" : np.sqrt((error ** 2).mean()),
+             "InfError" : np.linalg.norm(error, ord=np.inf),
              "ConditionC" : interp.condC,
              "Testfunction" : str(testfunction),
-             "m" : m if bf.has_shape_param else 0}
+             "m" : m}
 
 
 def main():
-    # mesh_sizes = np.linspace(10, 5000, num = 50)
-    # mesh_sizes = np.linspace(10, 200, num = 2)
-    mesh_sizes = np.logspace(10, 14, base = 2, num = 40) # = [1024, 16384]
-
+    parallel = True
+    workers = 2
+    writeCSV = False
     
-    bfs = [basisfunctions.Gaussian(), basisfunctions.ThinPlateSplines(),
-           basisfunctions.VolumeSplines(), basisfunctions.MultiQuadrics()]
+    # mesh_sizes = np.linspace(10, 5000, num = 50)
+    # mesh_sizes = np.linspace(10, 200, num = 2, dtype = int)
+    # mesh_sizes = np.logspace(10, 14, base = 2, num = 40) # = [1024, 16384]
+    # mesh_sizes = np.geomspace(100, 15000, num = 10)
+
+    mesh_sizes = np.array([100, 200, 300])
+
+    bfs = [basisfunctions.Gaussian, basisfunctions.ThinPlateSplines,
+           basisfunctions.VolumeSplines, basisfunctions.MultiQuadrics,
+           basisfunctions.CompactPolynomialC0, basisfunctions.CompactThinPlateSplineC2]
     RBFs = [rbf.NoneConsistent, rbf.SeparatedConsistent]
     tfs = [testfunctions.highfreq(), testfunctions.lowfreq(), testfunctions.jump()]
     ms = [4, 6, 8, 10, 14]
-    
-    params = []
 
+    params = []
     for mesh_size, RBF, bf, tf, m in itertools.product(mesh_sizes, RBFs, bfs, tfs, ms):
         if (not bf.has_shape_param) and (m != ms[0]):
             # skip iteration when the function has no shape parameter and it's not the first iteration in m
@@ -48,20 +58,35 @@ def main():
         params.append((mesh_size, RBF, bf, tf, m))
 
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers = 10) as executor:
-        result = executor.map(kernel, params)
-    # for p in params:
-        # kernel(**p)
+    # params = itertools.chain(
+        # itertools.product(mesh_sizes, RBFs,
+        #                   [basisfunctions.ThinPlateSplines, basisfunctions.VolumeSplines],
+        #                   tfs, [0]),
+        # itertools.product(mesh_sizes, RBFs,
+        #                   [basisfunctions.Gaussian],
+        #                   tfs, [4, 6, 8, 10, 14]),
+        # itertools.product(mesh_sizes, RBFs,
+                          # [basisfunctions.MultiQuadrics],
+                          # tfs, [0.1, 0.5, 1, 1.5])
+    # )
 
-
+    if parallel:
+        with concurrent.futures.ProcessPoolExecutor(max_workers = workers) as executor:
+            result = executor.map(kernel, params)
+    else:
+        result = []
+        for p in params:
+            result.append(kernel(p))
+        
     df = pd.DataFrame(list(result))
     df = df.set_index("h")
 
     df.to_pickle("h_convergence.pkl")
-    df.to_csv("h_convergence.csv")
 
-    for name, group in df.groupby(["RBF", "BF", "Testfunction", "m"]):
-        group.to_csv("h_convergence_" + "_".join(str(g) for g in name) + ".csv")
+    if writeCSV:
+        df.to_csv("h_convergence.csv")
+        for name, group in df.groupby(["RBF", "BF", "Testfunction", "m"]):
+            group.to_csv("h_convergence_" + "_".join(str(g) for g in name) + ".csv")
 
     print(df)
 
